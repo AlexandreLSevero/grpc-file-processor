bf0ef0b89528814b5ff4a63fa3f2fc16e3ea7c17
Alexandre Luppi <124376713+AlexandreLSevero@users.noreply.github.com>
2025-10-23 14:59:39 -0300

    Update server.cc

--- server_cpp/server.cc
+++ server_cpp/server.cc
@@ -2,14 +2,28 @@
 #include "file_processor.grpc.pb.h"
 #include "file_processor_service_impl.h"
 
+// Função para iniciar o servidor gRPC
 void RunServer() {
+  // endereço e porta onde o servidor vai ouvir
   std::string server_address("0.0.0.0:50051");
+
+  // Instancia a implementação do serviço
   FileProcessorServiceImpl service;
+
+  // Configura o servidor com gRPC
   grpc::ServerBuilder builder;
+
+  // Adiciona a porta de escuta com credenciais inseguras
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
+  // Registra o serviço no servidor
   builder.RegisterService(&service);
+
+  // Constrói e inicia o servidor
   std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
   std::cout << "Servidor gRPC ouvindo em " << server_address << std::endl;
+
+  // Mantém o servidor rodando até ser interrompido
   server->Wait();
 }
 
